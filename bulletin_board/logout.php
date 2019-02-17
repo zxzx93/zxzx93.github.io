82b003d6482fc04c5bd1ad9aadd8210432e642a3
zxzx93 <thddustn26@hanmail.net>
2019-02-17 12:59:32 +0900

    no message

--- bulletin_board/logout.php
+++ bulletin_board/logout.php
@@ -1,10 +1,10 @@
 <?php
   session_start();
   
-  //세션 제거
+
   unset($_SESSION['Member']);
 
-  //세션 제거 확인 후 메인페이지로 이동
+
   if(!isset($_SESSION['Member'])){
     header("Location:./index.php");
   }
